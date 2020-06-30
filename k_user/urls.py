59ed9097362b73b31e71e903f277b06912c373d6
pranpreten1 <pranpreten@naver.com>
2020-06-30 17:41:27 +0900

    aa

--- k_user/urls.py
+++ k_user/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
+    path('login/', views.login_page, name='login_page'),
 ]
--- k_user/views.py
+++ k_user/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def main_page(request):
     return render(request, 'k_user/main_page.html', {})
+
+
+def login_page(request):
+    return render(request, 'k_user/login_page.html', {})

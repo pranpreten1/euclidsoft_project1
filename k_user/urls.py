a60456c02b3bca16ea0665d67f9fa5ad97abe086
pranpreten1 <pranpreten@naver.com>
2020-07-01 11:55:24 +0900

    aaa

--- k_user/urls.py
+++ k_user/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.main_page, name='main_page'),
     path('login/', views.login_page, name='login_page'),
+    path('logout/', views.logout_page, name='logout_page'),
+    path('signup/', views.signup_page, name='signup_page'),
 ]
--- k_user/views.py
+++ k_user/views.py
@@ -1,9 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 def main_page(request):
     return render(request, 'k_user/main_page.html', {})
 
 
+def signup_page(request):
+    if request.method == "POST":
+        if request.POST["password1"] == request.POST["password2"]:
+            user = User.objects.create_user(
+                username=request.POST["username"],password=request.POST["password1"]
+            )
+            auth.login(request, user)
+            return redirect('main_page')
+        return render(request, "k_user/signup_page.html")
+
+
 def login_page(request):
-    return render(request, 'k_user/login_page.html', {})
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("main_page")
+        else:
+            return render(request, "k_user/login_page.html", {'error': 'username or password is incorrect'})
+
+    else:
+        return render(request, "k_user/login_page.html")
+
+
+def logout_page(request):
+    auth.logout(request)
+    return redirect("main_page")
